8c3ad7bdc53c273858ce71d4f5cb635226981e1d
Chetan Anand <canand2003@gmail.com>
2025-02-10 15:42:56 +0530

    Lec4_10thFeb

---
+++ Lec4_10_2025/index.js
@@ -0,0 +1,60 @@
+// console.log("start");
+
+// var a=10;
+ 
+// function sqofTwoNumbers(x){
+//     var ans= x*x;
+//     return ans;
+// }
+// console.log(sqofTwoNumbers(a));
+
+// console.log("end");
+
+/////////////////////////////////////////
+
+// console.log(abc);
+// stwo(10,5);
+// var abc=29;
+// function stwo(a,b){
+//     var ans=a+b;
+//     return ans;
+// }
+// var majAns=stwo(6,8);
+// console.log(majAns);  
+/////////////////////////////////////////////
+// console.log(xyz);
+// console.log(stwo(10,16));
+// let xyz=12;
+// function stwo(a,b){
+//     var ans=a+b;
+//     return ans;
+// }
+// console.log(stwo(19,27));
+
+///////////////////////////////////////////////////////
+
+var a=20;
+var b=30;
+var c=50;
+console.log(a);
+console.log(b);
+console.log(c);
+{
+    var a=5;
+   /// var b=4;
+    var c=18;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    {
+        var a=87;
+        var b=23;
+       // var c=43;
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    }
+}
+console.log(a);
+console.log(b);
+console.log(c);
