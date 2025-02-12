d04caa7e00bf697cccd9d9ade64f0836f5938480
Chetan Anand <canand2003@gmail.com>
2025-02-12 15:36:23 +0530

    Lec6_12_2025

---
+++ Lec6_12_2025/index.js
@@ -0,0 +1,65 @@
+//setTimeout : interview questions
+
+// console.log(1);
+// console.log(sum(10,5));
+// function sum(a,b){
+//     return a+b;
+// }
+// setTimeout(() => {
+//    console.log(4); 
+// }, 2000);
+// var start=1;
+// var end=100000;
+// while(start<end){
+//     start++;
+// }
+// console.log("end");
+// setInterval(() => {
+//     console.log(9);
+// }, 4000);
+
+
+
+//type of functions
+
+//statment function and named function
+
+// var a=10;
+// var b=10;
+// function sum(a,b){
+//     return a+b;
+// }
+
+//anonymous function and function expression
+
+// const ans=function(){
+//     return a+b;
+// }
+
+//arrow function
+
+// const ooo=(a,b)=>a+b;
+
+// console.log(sum(a,b))
+// console.log(ans());
+// console.log(ooo(10,10));
+
+
+// xyz();
+// abc();
+// function xyz(){
+//     console.log(1);
+// }
+// var abc=function(){
+//     console.log(2);
+// }
+
+
+function xyz(){
+    console.log(1);
+}
+var abc=function cdf() {
+    console.log(2);
+}
+xyz();
+abc();
